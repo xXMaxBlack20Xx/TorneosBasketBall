0f182aafecbae836498966264ca0f631d1125f5c
Rodrigo Maximo Trigo Gonzalez - 375092 <spadatyma@gmail.com>
2025-06-08 02:11:54 -0700

    Ahora ya se tiene el Home page junto a un sistema basico de guardado de datos.

---
+++ TorneosBasketBall/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TorneosBasketBall.Models;
+using TorneosBasketBall.Servicios;
+
+namespace TorneosBasketBall.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ContextoBD _context;
+
+        public HomeController(ContextoBD context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Llenar el ViewModel con los datos de la base de datos
+            var vm = new DashboardViewModel
+            {
+                Equipos = _context.Equipos.ToList(),
+                Jugadores = _context.Jugadores.ToList(),
+                Partidos = _context.Partidos.ToList(),
+                Estadisticas = _context.EstadisticasEquipos.ToList()
+            };
+            return View(vm); // Busca Views/Home/Index.cshtml
+        }
+    }
+}
---
+++ TorneosBasketBall/Models/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TorneosBasketBall.Models
+{
+    public class DashboardViewModel
+    {
+        public required List<Equipo> Equipos { get; set; }
+        public required List<Jugador> Jugadores { get; set; }
+        public required List<Partidos> Partidos { get; set; }
+        public required List<EstadisticasEquipos> Estadisticas { get; set; }
+    }
+
+}
